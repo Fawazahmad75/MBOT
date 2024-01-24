ad893de47dce09f33108186cc7acecfcd7b58be1
Fawazahmad75 <114078421+Fawazahmad75@users.noreply.github.com>
2024-01-24 10:30:52 -0330

    Add files via upload

---
+++ AI Image Generator/MBOT/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'  
+import './ImageGenerator.css'
+import default_image from '../Assests/default_image.svg'
+const ImageGenerator=()=>{
+return(
+    <div className='ai-image-generator'>
+        <div className="header">AI image<spane>generator</spane></div>
+       
+    </div>
+)
+}
+export default ImageGenerator
